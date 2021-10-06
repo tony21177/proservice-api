284d1db91177f91026bfe125c54ddba021cd970c
kun <tony211776@hotmail.com>
2021-10-06 21:27:56 +0800

    add router protect

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,9 +1,9 @@
 require('../firebase')
 const {getAuth,signInWithEmailAndPassword} = require('firebase/auth')
+const {firebaseAdmin} = require('../firebase')
 
 exports.authUserEmailAndPass = async(email,password)=>{
     console.log("authUserEmailAndPass..")
-    console.log("email:%s,password:%s",email,password)
     try{
         const auth = getAuth()
         const userCredential = await signInWithEmailAndPassword(auth,email,password)
@@ -16,3 +16,48 @@ exports.authUserEmailAndPass = async(email,password)=>{
     }
     
 }
+
+exports.tokenProtect = async (req, res, next) => {
+    try {
+        // 1) check if the token is there
+        let token;
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith("Bearer")
+        ) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+        if (!token) {
+            res.status(401).json({
+                success: false,
+                data: {
+                    msg: "沒有權限"
+                },
+            })
+            return
+        }
+
+        // 2) Verify token
+        firebaseAdmin.auth()
+        .verifyIdToken(token)
+        .then((decodedToken) => {
+            const uid = decodedToken.uid;
+            // ...
+            req.email = decodedToken.email
+            next();
+        })
+        .catch((error) => {
+            res.status(401).json({
+                success: false,
+                data: {
+                    msg: error.message
+                },
+            })
+            // Handle error
+        });
+
+        
+    } catch (err) {
+        next(err);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const eventProcesser = require('../service/eventProcesser')
 const authService = require('../service/authService')
+const {tokenProtect} = require('../auth/authenticate')
 
 // restful api for mqtt
 router.post("/api/v1/event",eventProcesser.saveEvent);
 // restful api for scroll events list
-router.post("/api/v1/event/list",eventProcesser.scrollEvents)
+router.post("/api/v1/event/list",tokenProtect,eventProcesser.scrollEvents)
 router.post("/api/v1/event/sync",eventProcesser.syncEvents)
 
 // restful api for text/plain
