e09208f2d75538a18da9aa37cc8f21c30034b336
kun <kunnan_lin@Neithnet.com>
2021-10-06 16:35:07 +0800

    init admin

--- datastore/postgres/users.js
+++ datastore/postgres/users.js
@@ -4,7 +4,7 @@ const db = require('./index')
 exports.insertUserOrUpdateLastLoginTime = async (uid,email,role,authType) => {
     let result="";
     try {
-        result = await db.query('insert into users(uid,email,role,authType) values($1,$2,$3,$4)  ON CONFLICT (email) DO UPDATE SET "lastLoginTime" =now() returning id', [uid,email,role,authType]);
+        result = await db.query('insert into users(uid,email,role,"authType") values($1,$2,$3,$4)  ON CONFLICT (email) DO UPDATE SET "lastLoginTime" =now() returning id', [uid,email,role,authType]);
     } catch (e) {
         console.log("insertUserOrUpdateLastLoginTime error", e.message);
         throw e;
@@ -16,7 +16,7 @@ exports.insertUserOrUpdateLastLoginTime = async (uid,email,role,authType) => {
 exports.insertUser = async (uid,email,role,authType) => {
     let result="";
     try {
-        result = await db.query('insert into users(uid,email,role,authType) values($1,$2,$3,$4)', [uid,email,role,authType]);
+        result = await db.query('insert into users(uid,email,role,"authType") values($1,$2,$3,$4)', [uid,email,role,authType]);
     } catch (e) {
         console.log("insertUser error", e.message);
         throw e;
