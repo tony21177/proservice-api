e66a8d3b91ff34746b56f5ef86c931dbc90bccbd
kun <tony211776@hotmail.com>
2021-10-12 22:25:24 +0800

    add table user_fcm_token

---
+++ migrations/1634047849749_user-fcm-token.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('user_fcm_token', {
+        id: 'id',
+        uid: { type: 'varchar(1000)', notNull: true },
+        fcmToken: { type: 'varchar(300)', notNull: true },
+        envId: { type: 'varchar(100)' },
+        envLocation: { type: 'varchar(100)' },
+        envInfo: { type: 'text' },
+        createdAt: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        }
+    })
+    pgm.addConstraint(user_fcm_token, 'unique_uid_token', 'UNIQUE (uid,fcmToken)')
+
+};
+
+exports.down = pgm => { };
